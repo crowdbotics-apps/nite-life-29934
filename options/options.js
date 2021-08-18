83bb01e4e6188a6095b0c91d4cf743616e013121
Crowdbotics <team@crowdbotics.com>
2021-08-18 23:05:29 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "nite_life_29934Identifier",
+  url: "https://nite_life_29934Identifier.botics.co",
+  api: "https://nite_life_29934Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
